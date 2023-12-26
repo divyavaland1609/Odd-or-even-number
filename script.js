7b8f34366d2aeb5d9471225105b27430bd11e916
divyavaland1609 <divyavaland479@gmail.com>
2023-12-26 13:34:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function odd_even(){
+    var  a;
+    a = Number(document.getElementById('input_num').value);
+    if(a == 0){
+        document.write("Even Number");
+    }
+    else{
+        document.write("Odd Number");
+    }
+
+
+}
